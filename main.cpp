a03f12a64eb2ebfc3f2bed7f7cf871aa60f632fe
Daechul Park <dcpark1992@gmail.com>
2019-09-02 17:30:28 +0900

    tasks creation with pthread and sched_deadline

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include <pthread.h>
 #include <unistd.h>
 
 #include <iostream>
 #include <fstream>
 
 // open-source libraries
+#include "sched_deadline.hpp"
 #include "json.hpp"
 
 // custom libraries
@@ -12,6 +14,8 @@
 
 using namespace std;
 using json = nlohmann::json;
+
+pthread_t thread[16];
 
 int main(int argc, char **argv)
 {
@@ -24,6 +28,17 @@ int main(int argc, char **argv)
 
 	int strategy = get_strategy(config);
 	int num_task = get_num_task(config);
+
+	int thread_cnt = 0;
+	for(int i = 1; i <= num_task; i++) {
+			pthread_create(&thread[thread_cnt], 0, run_deadline, arg);
+			thread_cnt++;
+	}
+
+	for(int i = 0; i < thread_cnt; i++) {
+		pthread_join(thread[i], NULL);
+	}
+	cout << "main dies: " << gettid() << endl;
 
 	return 0;
 }	
