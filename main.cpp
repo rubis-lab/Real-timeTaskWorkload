6df03bfdf079646ea2698c478beb2ef0416c2a80
Daechul Park <dcpark1992@gmail.com>
2019-09-02 15:29:01 +0900

    json input file parser

--- main.cpp
+++ main.cpp
@@ -3,11 +3,20 @@
 #include <iostream>
 #include <fstream>
 
+// open-source libraries
+#include "json.hpp"
+
 using namespace std;
+using json = nlohmann::json;
 
 int main(int argc, char **argv)
 {
 	cout << "main thread id: " << gettid() << endl;
+
+	// parse config file
+	json config;
+	ifstream input_file("config.json");
+	input_file >> config;
 
 	return 0;
 }	
